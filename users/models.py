6132c7d0e1ab96d6dabc69b46d38c3943585e31d
Nihal <37813784+iamnihal@users.noreply.github.com>
2021-09-18 23:40:44 +0530

    Update models.py
    Changed email field property to unique

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# To make email field unique
+User._meta.get_field('email')._unique = True
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
